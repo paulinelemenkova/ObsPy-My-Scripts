9b586657d786c3a2f9b2151299a6a7cd36318c49
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2022-02-28 09:26:11 +0100

    Add files via upload

--- Script-OBSPY.py
+++ Script-OBSPY.py
@@ -4,24 +4,6 @@ python3
 import os
 os.chdir('/Users/polinalemenkova/Documents/Python/OBSPY')
 # pip install obspy
-from obspy import read
-#st = read("traces_UCC19540108Gal_N_0815.mseed")
-#st = read("traces_UCC19540130Gal_N_0808.mseed")
-#st = read("traces_UCC19540130Gal_E_0808.mseed")
-#st = read("")
-st = read("traces_UCC19540111Gal_N_0824.mseed")
-print(st)
-st = read("traces_UCC19540312Gal_E_0834.mseed")
-print(st)
-st = read("traces_UCC19540311Gal_E_0727.mseed")
-print(st)
-st = read("traces_UCC19540311Gal_E_0727.mseed")
-print(st)
-st = read("traces_UCC19540213Gal_N_0816.mseed")
-print(st)
-st = read("traces_UCC19540213Gal_E_0816.mseed")
-print(st)
-
 #--------------------------------------------------#
 
 # 1.2 UTCDateTime
@@ -43,4 +25,35 @@ time = UTCDateTime("2012-09-07T12:15:00")
 time2 = UTCDateTime(2012, 1, 1)
 print(time - time2)
 
-# 1.2.4 Exercises
+#--------------------------------------------------#
+# 1.3 Reading Seismograms
+from obspy import read
+#st = read("traces_UCC19540108Gal_N_0815.mseed")
+#st = read("traces_UCC19540130Gal_N_0808.mseed")
+#st = read("traces_UCC19540130Gal_E_0808.mseed")
+#st = read("")
+st = read("traces_UCC19540111Gal_N_0824.mseed")
+print(st)
+st = read("traces_UCC19540312Gal_E_0834.mseed")
+print(st)
+st = read("traces_UCC19540311Gal_E_0727.mseed")
+print(st)
+st = read("traces_UCC19540311Gal_E_0727.mseed")
+print(st)
+st = read("traces_UCC19540213Gal_N_0816.mseed")
+print(st)
+st = read("traces_UCC19540213Gal_E_0816.mseed")
+print(st)
+
+# 1.3.1 Accessing Meta Data (location, station, etc.)
+tr = st[0] # assign first and only trace to new variable
+print(tr)
+print(tr.stats)
+tr.stats.station
+tr.stats.mseed.datatype
+
+# 1.3.2 Accessing Waveform Data
+# Retrieve the actual waveform data via the data keyword on each Trace:
+tr.data
+tr.data[0:3]
+len(tr)
